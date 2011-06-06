626410244bc65e5481616645695e451db013d8de
matthieu35@aliceadsl.fr <matthieu35@aliceadsl.fr@5ace9468-2222-0c94-631f-89218e435bca>
2011-06-06 09:15:23 +0000

    Updated to v1.7. Now works with PTD up to v2.8 alpha.
    Changes related to v2.8 alpha :
    
     * Added an option to edit achievements
     * Added Shiny Charmander event
    
    Other changes :
    
     * Now displays the Pokémon's sprite
     * Moved some elements on the UI
     * Now automatically saves the Pokémon's data when changing in the list
     * Added a button to cancel changes made to the current Pokémon

---
+++ trunk/French/PTD Save Editor/Form4.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Text
+
+Public Class Form4
+
+    Private Sub Form4_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim tmpCb As CheckBox
+        For i As Integer = 0 To Form1.currentAchievements.Length - 1
+            tmpCb = CType(gb_Achieve1.Controls.Find("cb_Achieve1_" & (i + 1), False)(0), CheckBox)
+            tmpCb.Checked = Form1.currentAchievements(i) <> "0"c
+
+            tmpCb.Enabled = Form1.achievements(i) = "0"c
+        Next i
+    End Sub
+
+    Private Sub b_Validate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Validate.Click
+        Dim sb As New StringBuilder(gb_Achieve1.Controls.Count())
+        sb.Append("0"c, gb_Achieve1.Controls.Count())
+
+        For Each c As CheckBox In gb_Achieve1.Controls
+            sb(CInt(c.Tag)) = If(c.Checked, "1"c, "0"c)
+        Next c
+
+        Form1.currentAchievements = sb.ToString()
+
+        Close()
+    End Sub
+End Class
