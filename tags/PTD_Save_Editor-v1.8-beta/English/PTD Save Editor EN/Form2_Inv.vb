932eff8df2fe516a42db7e7c4e658481d9cd55e2
matthieu35@aliceadsl.fr <matthieu35@aliceadsl.fr@5ace9468-2222-0c94-631f-89218e435bca>
2011-06-11 23:42:41 +0000

    Tagged v1.8 beta (French & English).

---
+++ tags/PTD_Save_Editor-v1.8-beta/English/PTD Save Editor EN/Form2_Inv.vb
@@ -0,0 +1,78 @@
+﻿Public Class Form2_Inv
+
+    Friend tmpItems, tmpItemCount As List(Of Integer)
+
+    Private Sub refreshItems()
+        tmpItemCount = New List(Of Integer)()
+
+        For i As Integer = 1 To Form1_Main.ItemList.Count - 1
+            tmpItemCount.Add(0)
+        Next i
+
+        For Each i As Integer In tmpItems
+            If i > 0 Then
+                tmpItemCount(i - 1) += 1
+            End If
+        Next i
+
+        gb_Items.Controls.Clear()
+
+        For i As Integer = 0 To tmpItemCount.Count - 1
+            Dim lbl As New Label()
+            lbl.AutoSize = True
+            lbl.Text = Form1_Main.ItemList(i + 1)
+            lbl.Location = New Drawing.Point(6, 26 * i + 21)
+
+            Dim nud As New NumericUpDown()
+            nud.Minimum = 0
+
+            Dim max As Integer = If(i = 5, 1, 99)
+
+            nud.Maximum = Math.Max(max, tmpItemCount(i))
+
+            nud.Value = tmpItemCount(i)
+            nud.Location = New Drawing.Point(85, 26 * i + 19)
+            nud.Width = 40
+            nud.Tag = i + 1
+
+            gb_Items.Controls.AddRange(New Control() {lbl, nud})
+        Next i
+    End Sub
+
+    Private Sub updateItems()
+        Dim tmpNud As NumericUpDown
+        tmpItems = New List(Of Integer)()
+
+        For Each c As Control In gb_Items.Controls
+            If TypeOf c Is NumericUpDown Then
+                tmpNud = CType(c, NumericUpDown)
+
+                For i As Integer = 1 To tmpNud.Value
+                    tmpItems.Add(CInt(tmpNud.Tag))
+                Next i
+            End If
+        Next c
+    End Sub
+
+    Private Sub Form2_Inv_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        tmpItems = New List(Of Integer)(Form1_Main.tmpInv)
+
+        refreshItems()
+
+        Me.Height = 26 * tmpItemCount.Count + 127
+        Me.Top = Form1_Main.Top + (Form1_Main.Height - Me.Height) / 2
+    End Sub
+
+    Private Sub b_Validate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Validate.Click
+        updateItems()
+
+        Form1_Main.tmpInv = New List(Of Integer)(tmpItems)
+        Close()
+    End Sub
+
+    Private Sub b_AdvancedMode_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_AdvancedMode.Click
+        updateItems()
+        Form2_Inv_Adv.ShowDialog()
+        refreshItems()
+    End Sub
+End Class
---
+++ tags/PTD_Save_Editor-v1.8-beta/English/PTD Save Editor EN/Form2_Inv_Adv.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form2_Inv_Adv
+
+    Private tmpItems As List(Of Integer)
+
+    Private Sub Form2_Inv_Adv_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        tmpItems = New List(Of Integer)(Form2_Inv.tmpItems)
+
+        lb_Items.Items.Clear()
+        For Each i As Integer In tmpItems
+            lb_Items.Items.Add(Form1_Main.ItemList(i))
+        Next i
+
+        lb_Items.SelectedIndex = -1
+        cb_ItemList.SelectedIndex = -1
+        cb_ItemList.Enabled = False
+        b_Modify.Enabled = False
+        b_Delete.Enabled = False
+    End Sub
+
+    Private Sub b_Delete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Delete.Click
+        Dim tmpIndex As Integer = lb_Items.SelectedIndex
+
+        tmpItems.RemoveAt(tmpIndex)
+        lb_Items.Items.RemoveAt(tmpIndex)
+
+        lb_Items.SelectedIndex = If(tmpIndex < lb_Items.Items.Count, tmpIndex, lb_Items.Items.Count - 1)
+
+        lb_Items.Focus()
+    End Sub
+
+    Private Sub lb_Items_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles lb_Items.KeyDown
+        If e.KeyCode = Keys.Delete Then
+            b_Delete.PerformClick()
+        ElseIf e.KeyCode = Keys.Insert Then
+            b_Add.PerformClick()
+        End If
+    End Sub
+
+    Private Sub lb_Items_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lb_Items.SelectedIndexChanged
+        If lb_Items.SelectedIndex < 0 Then
+            cb_ItemList.SelectedIndex = -1
+            cb_ItemList.Enabled = False
+            b_Modify.Enabled = False
+            b_Delete.Enabled = False
+        Else
+            cb_ItemList.SelectedIndex = tmpItems(lb_Items.SelectedIndex)
+            cb_ItemList.Enabled = True
+            b_Modify.Enabled = True
+            b_Delete.Enabled = True
+        End If
+    End Sub
+
+    Private Sub b_Validate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Validate.Click
+        Form2_Inv.tmpItems = New List(Of Integer)(tmpItems)
+    End Sub
+
+    Private Sub b_Modify_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Modify.Click
+        tmpItems(lb_Items.SelectedIndex) = cb_ItemList.SelectedIndex
+        lb_Items.Items(lb_Items.SelectedIndex) = Form1_Main.ItemList(cb_ItemList.SelectedIndex)
+    End Sub
+
+    Private Sub b_Add_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles b_Add.Click
+        tmpItems.Add(If(cb_ItemList.SelectedIndex < 1, 0, cb_ItemList.SelectedIndex))
+        lb_Items.Items.Add(Form1_Main.ItemList(If(cb_ItemList.SelectedIndex < 1, 0, cb_ItemList.SelectedIndex)))
+        lb_Items.SelectedIndex = lb_Items.Items.Count - 1
+    End Sub
+End Class
